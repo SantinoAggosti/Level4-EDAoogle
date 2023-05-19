3f5cb0a03001da106b67947fe4da26275ccd19ab
SantinoAggosti <97355841+SantinoAggosti@users.noreply.github.com>
2023-05-18 21:59:44 -0300

    Initial Commit

---
+++ CommandLineParser.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file CommandLineParser.h
+ * @author Marc S. Ressl
+ * @brief Quick and dirty command line parser
+ * @version 0.2
+ *
+ * @copyright Copyright (c) 2022-2023
+ *
+ */
+
+#include "CommandLineParser.h"
+
+using namespace std;
+
+CommandLineParser::CommandLineParser(int argc, const char *argv[])
+{
+    arguments.assign(argv + 1, argv + argc);
+}
+
+bool CommandLineParser::hasOption(const string &name)
+{
+    for (auto &argument : arguments)
+    {
+        if (argument == name)
+            return true;
+    }
+
+    return false;
+}
+
+string CommandLineParser::getOption(const string &name)
+{
+    for (auto i = arguments.begin(); i != arguments.end(); i++)
+    {
+        if (*i == name)
+        {
+            if ((i + 1) != arguments.end())
+                return *(i + 1);
+        }
+    }
+
+    return "";
+}
---
+++ CommandLineParser.h
@@ -0,0 +1,29 @@
+/**
+ * @file CommandLineParser.h
+ * @author Marc S. Ressl
+ * @brief Quick and dirty command line parser
+ * @version 0.2
+ *
+ * @copyright Copyright (c) 2022-2023
+ *
+ */
+
+#ifndef COMMANDLINEPARSER_H
+#define COMMANDLINEPARSER_H
+
+#include <string>
+#include <vector>
+
+class CommandLineParser
+{
+public:
+    CommandLineParser(int argc, const char *argv[]);
+
+    bool hasOption(const std::string &option);
+    std::string getOption(const std::string &option);
+
+private:
+    std::vector<std::string> arguments;
+};
+
+#endif
